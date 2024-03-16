124a6c83208e9c3125f62cbe756a478ed17cccc2
damian orina <96182161+Mianse@users.noreply.github.com>
2024-03-17 02:46:22 +0300

    Create Product API React Native Ecommerce App Mern Stack Project Mern App

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,4 +1,6 @@
 import productModel from "../models/productModel.js"
+import { getDataUri } from "../utils/Feature.js"
+import cloudinary from "cloudinary"
 
 export const getAllProductController =async(req,res)=>{
   try {
@@ -46,4 +48,38 @@ export const getSingleProductController =async(req,res)=>{
             message:"error when getting single product"
         })
     }
+}
+
+export const createProductController =async(req,res)=>{
+try {
+    const {name,price,description,category,stock}= req.body;
+    //validation 
+   
+    if(!req.file){
+        res.status(400).send({
+            success: false,
+            message: "please provide product image "
+        })
+    }
+    const file = getDataUri(req.file);
+    const cdb = await cloudinary.v2.uploader.upload(file.content)
+    const image = {
+        public_id: cdb.public_id,
+        url: cdb.secure_url
+    }
+    await productModel.create({
+        name, price, description, category, stock,images: [image]
+    })
+    res.status(201).send({
+        success:  true,
+        message: "product created successfully"
+    })
+} catch (error) {
+    console.log(error)
+    res.status(500).send({
+        success: false,
+        message: "error in creating  new product",
+        error
+    })
+}
 }
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getAllProductController,getSingleProductController } from "../controllers/productController.js";
-
+import { getAllProductController,getSingleProductController, createProductController} from "../controllers/productController.js";
+import { isAuth } from "../middlewares/authMiddleware.js";
+import { singleUpload } from "../middlewares/multer.js";
 const router = express.Router()
 //get all products
 router.get("/get-all",getAllProductController)
 //get single product
 router.get("/:id",getSingleProductController)
+
+//create product route
+router.post("/create",isAuth,singleUpload,createProductController)
 export default router
