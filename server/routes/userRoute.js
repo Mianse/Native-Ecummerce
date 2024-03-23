c6baa6b072ecccbeb689b4e5a2680a2bd7e19bcd
damian orina <96182161+Mianse@users.noreply.github.com>
2024-03-23 23:31:08 +0300

    Redux Toolkit Setup React Native Ecommerce App Mern Stack Project Mern App

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,12 +2,24 @@ import express from 'express'
 import { registerController,loginController ,getUserProfileController,logoutController,passwordResetController,updateUserProfileController,updatePasswordController,updateProfilePicController} from '../controllers/userController.js';
 import  {isAuth} from '../middlewares/authMiddleware.js';
 import { singleUpload } from '../middlewares/multer.js';
+import {rateLimit} from 'express-rate-limit';
 
+
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+
+// Apply the rate limiting middleware to all requests.
 const router = express.Router()
 
-router.post("/register",registerController)
+router.post("/register",limiter,registerController)
 //LOGIN
-router.post("/login",loginController)
+router.post("/login",limiter,loginController)
 
 //getuser router
 router.get("/profile",isAuth,getUserProfileController)
